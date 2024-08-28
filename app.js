6e126d82177fbfe6354caf4f2bf77f5c20c1a327
aideity <luchesimartina@gmail.com>
2024-08-28 19:58:39 -0300

    Add files via upload
    error en el console log, hay que añadir un parse json en vez de require?

---
+++ app.js
@@ -0,0 +1,37 @@
+const doc = require('./collectibles');
+
+doc.importarDatos(doc.HotToys);
+
+let unifiedCollectibles = [...doc.HotToys, ...doc.Bandai, ...doc.StarWars];
+
+
+let collectibles = {
+    figura: unifiedCollectibles,
+
+    // Método para listar todas las figuras
+    listFigures: function() {
+        this.figura.forEach(function(figura, index) {
+            console.log(`${index + 1}.- ${figura.nombre}`); 
+        });
+    },
+
+    figuresByBrand: function(brand) {
+        return this.figura.filter(figura => figura.marca === brand);
+    }
+};
+        
+console.log(collectibles);
+
+console.log("Todas las figuras:");
+collectibles.listFigures();
+
+console.log('Figuras de la marca HotToys');
+
+console.log(collectibles.figuresByBrand('HotToys'));
+
+console.log("Figuras de la marca 'Bandai':");
+console.log(collectibles.figuresByBrand('Bandai'));
+
+console.log("Figuras de la marca 'StarWars':");
+let starWarsFigures = collectibles.figuresByBrand('StarWars');
+console.log(starWarsFigures);
